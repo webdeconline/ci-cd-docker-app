746a4ae4db8acf914bf0f8e5ae255a11c5362f74
webdeconline <webdeconline@gmail.com>
2025-08-17 23:18:57 +0200

    update project

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello, Docker! My name is Daniel Stanciu student at UCLL Leuven.'
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
